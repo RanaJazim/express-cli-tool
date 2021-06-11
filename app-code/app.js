ede23579c11653da9c0cf9d09ce69d99d34f9e8a
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 14:53:19 +0500

    added routes setup ..

--- app-code/app.js
+++ app-code/app.js
@@ -3,11 +3,13 @@ require("express-async-errors");
 const bodyParser = require("body-parser");
 
 // local imports
+const allRoutes = require("./src/routes");
 const catchUnhandleExceptions = require("./src/middlewares/exception-handling");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", allRoutes);
 
 // this should be always the last middleware. otherwise it will not work properly
 // best practise don't modify this middleware
