52d9fad762c3fb652b832b9fe413fec44fb91e8d
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 13:37:53 +0500

    scaffolding very basic setup. this is just for testing purposes ..

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("cli for express scaffolding");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    { name: "projectName", message: "Your project name:", type: "input" },
+  ])
+  .then((answers) => {
+    const content = "Content";
+
+    const dirPath = process.cwd() + "/sample/hello";
+    createDirIfNotExists(dirPath);
+    const filePath = dirPath + "/first.txt";
+
+    fs.appendFile(filePath, content, (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      console.log(res);
+    });
+
+    console.log(answers);
+  });
+
+function createDirIfNotExists(dirName) {
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName, { recursive: true });
+  }
+}
