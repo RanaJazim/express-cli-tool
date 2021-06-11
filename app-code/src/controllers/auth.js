f836e0911c10066229ec6df73bbe5eb7c6994c3c
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 16:19:10 +0500

    initial setup for auth controller and routes ..

---
+++ app-code/src/controllers/auth.js
@@ -0,0 +1,5 @@
+async function register(req, res) {}
+
+async function login(req, res) {}
+
+module.exports = { register, login };
--- app-code/src/routes/index.js
+++ app-code/src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const authRoutes = require("./auth");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({ message: "HomeRoute" });
 });
+router.use("/auth", authRoutes);
 
 module.exports = router;
