c9b67e01a8380839369a7a1293b990be5aac390a
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 15:14:09 +0500

    hash password in user model before saving to db ..

--- app-code/src/db/user.js
+++ app-code/src/db/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -8,6 +9,25 @@ const userSchema = new mongoose.Schema({
   },
   password: String,
 });
+
+userSchema.pre("save", hashPassword);
+function hashPassword(next) {
+  var user = this;
+
+  if (!user.password) return next();
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+}
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
