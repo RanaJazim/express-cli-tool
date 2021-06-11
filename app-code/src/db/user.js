151f4caacffc921b93c2449c63329541c723f11e
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 15:22:03 +0500

    generate jwt token and compare hashPassword with plain password string ..

--- app-code/src/db/user.js
+++ app-code/src/db/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const config = require("config");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -29,5 +30,25 @@ function hashPassword(next) {
   });
 }
 
+userSchema.method("comparePassword", async function (password) {
+  return await bcrypt.compare(password, this.password);
+});
+
+userSchema.method("generateToken", function (fields = {}) {
+  const userWithoutPassword = excludePasswordField(this);
+  const token = jwt.sign(
+    { ...userWithoutPassword, ...fields },
+    config.get("jwt.secret")
+  );
+  return token;
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
+
+function excludePasswordField(userModel) {
+  let userFields = userModel._doc;
+  const { password, userFields } = userFields;
+
+  return userFields;
+}
