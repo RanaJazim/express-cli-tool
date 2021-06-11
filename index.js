bd281e1dfccd61abb94de598e4fdc3e9c5b2524c
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-11 14:01:14 +0500

    copy app-code folder to some other folder. new destination folder is static now but fixed it later ..

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const fse = require("fs-extra");
+const path = require("path");
 
 inquirer
   .prompt([
     { name: "projectName", message: "Your project name:", type: "input" },
   ])
   .then((answers) => {
-    const content = "Content";
+    // const content = "Content";
 
     const dirPath = process.cwd() + "/sample/hello";
     createDirIfNotExists(dirPath);
-    const filePath = dirPath + "/first.txt";
+    // const filePath = dirPath + "/first.txt";
 
-    fs.appendFile(filePath, content, (err, res) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+    // fs.appendFile(filePath, content, (err, res) => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
 
-      console.log(res);
-    });
+    //   console.log(res);
+    // });
+
+    copy(dirPath);
 
     console.log(answers);
   });
@@ -28,4 +32,18 @@ function createDirIfNotExists(dirName) {
   if (!fs.existsSync(dirName)) {
     fs.mkdirSync(dirName, { recursive: true });
   }
+}
+
+function copy(destDir) {
+  //   const srcDir = "app-code";
+  const srcDir = path.resolve(__dirname, "./app-code");
+
+  fse.copySync(srcDir, destDir, { overwrite: true }, (err) => {
+    if (err) {
+      console.log("Error while scaffolding project", err);
+      return;
+    }
+
+    console.log("Successfully scaffolding ...");
+  });
 }
